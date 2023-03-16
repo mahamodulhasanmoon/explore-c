b7746864288b0f1e0fa848bb508f99f94bf2e227
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-03-17 00:53:43 +0600

    basic data type

---
+++ intro/dataType.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+int main(){
+
+// int variable 
+int num = 25;
+// float variable
+float num2 = 50.345f;
+// character variable
+char text = 'T'; //only one character
+
+// for number
+printf("%d\n", num);
+printf("%f\n", num2);
+printf("%c\n", text);
+
+
+    return 0;
+}
