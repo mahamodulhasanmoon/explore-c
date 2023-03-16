b9ba132aaea1ef6257d2096f56ff2ace26ec0c6b
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-03-17 00:03:09 +0600

    explore integer

---
+++ intro/variable.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+
+int main (){
+
+// learn variable
+  // for integer variables
+
+ int number =10;
+
+ printf("number + 5 = %d\n",number + 5);
+ printf("number - 5 = %d\n",number - 5);
+ printf("number * 5 = %d\n",number * 5);
+ printf("number / 5 = %d\n",number / 5);
+ 
+ 
+
+
+
+
+
+
+    return 0;
+}
