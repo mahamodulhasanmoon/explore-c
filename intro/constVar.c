e3190de01d71bf07bb637bfa07964236418d5295
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-03-17 01:13:45 +0600

    reserved keyword

---
+++ intro/constVar.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+// define constant variables
+
+// #define PI = 3.1416;
+
+// alternate constants
+
+// c programming Reserved keywords
+
+
+
+
+int main(){
+    const float PI = 3.1416f;
+
+ 
+    printf("%f\n", PI);
+
+    return 0;
+}
