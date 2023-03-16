bfd900f431f12e14c95fb9ebef2354a1222c37d0
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-03-17 02:08:17 +0600

    c programme basic calculation done

--- 02.Basic-project-with-core/calculation.c
+++ 02.Basic-project-with-core/calculation.c
@@ -8,8 +8,26 @@ int main() {
     // Calculate the sum
     result = num1 + num2;
 
-    // Display the result
+    // Display the sumation
 printf("%.2f + %.2f = %.2f\n", num1, num2, result);
+    // Calculate the substration
+    result = num1 - num2;
+
+    // Display the substration
+printf("%.2f - %.2f = %.2f\n", num1, num2, result);
+
+
+    // Calculate the Multiplication
+    result = num1 * num2;
+
+    // Display the substration
+printf("%.2f * %.2f = %.2f\n", num1, num2, result);
+
+    // Calculate the division
+    result = num1 / num2;
+
+    // Display the substration
+printf("%.2f / %.2f = %.2f\n", num1, num2, result);
 
     return 0;
 }
