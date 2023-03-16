da6b03ae3588d11114ccab556a1fed83a888d521
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-03-16 22:34:16 +0600

    done

--- intro/hello.c
+++ intro/hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    printf("hello world\n");
+    printf("hello world\n i am happy to explore it \n");
     return 0;
 }
 
---
+++ intro/print.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+int main(){
+printf("my Name is Moon\n");
+printf("I am A MERN Stack Developer\n");
+// for tab
+printf("Now i Am \texploring C programme\n");
+
+// sum
+
+printf("%d\n",5+4);
+printf("%d\n",5-4);
+printf("%d\n",5*4);
+
+    return 0;
+}
