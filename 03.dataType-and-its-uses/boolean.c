3368c4c560fd0f0abd411bf8eeecae55940ad2c9
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-03-18 00:55:53 +0600

    boolean

---
+++ 03.dataType-and-its-uses/boolean.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+# include <stdbool.h>
+
+int main(void) {
+    // explore boolean values
+    short is_logged_in = 0;
+  
+    // bool is_logged_in = true;
+  
+
+    if (is_logged_in)
+    {
+        printf("user Logged in \n");
+    }else{
+        printf("user Logged out \n");
+    }
+    
+
+    return 0;
+}
