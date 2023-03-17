1497e9c2d5ee43816a5672b719ab192fb481e201
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-03-18 00:26:04 +0600

    done

---
+++ 03.dataType-and-its-uses/char.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main(void) {
+
+    //sing character
+    char a = 90;
+    printf("char data : %c\n", a);
+
+    return 0; 
+}
---
+++ 03.dataType-and-its-uses/floating.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+int main(void){
+    // floating number 
+float f = 458.235f;
+printf("floating point number : %f\n", f);
+// / double 
+double d = 458.235;
+printf("double point number : %lf\n", d);
+
+// /long double
+long double lld = 1721515151.315;
+printf("long double point number : %Lf\n", lld);
+
+
+
+
+    return 0;
+}
