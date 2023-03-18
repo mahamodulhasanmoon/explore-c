8ca7b12e304f4918c2001db5bfd7bb92c89030fb
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-03-18 21:40:37 +0600

    assignment operator

---
+++ 04.operators/assignment.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+
+int main(void){
+int a = 12;
+
+// a = a + 5;
+
+// short 
+
+a+=10;
+
+
+printf("a = %d\n", a);
+a-=10;
+printf("a = %d\n", a);
+
+a*=10;
+printf("a = %d\n", a);
+
+a/=10; 
+printf("a = %d\n", a);
+
+a%=10; 
+printf("a = %d\n", a);
+
+    return 0;
+}
