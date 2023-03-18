4d7e07eaf59b633145f8fa5b161c278670cdc31a
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-03-18 21:31:43 +0600

    logical operator

---
+++ 01.intro/input.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+// & memory niye kaj kore
+
+int main(){
+
+float a,b;
+
+scanf("%f %f", &a,&b);
+printf("%f %f", a+b);
+
+
+    return 0;
+}
---
+++ 04.operators/boolean.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+# include <stdbool.h>
+
+
+int main(){
+
+bool have_nid = false;
+bool is_adult = false;
+bool has_passport = true;
+
+// bool is_permitted = has_identitiy && is_adult;
+
+bool is_permitted =(have_nid || has_passport)&& is_adult;
+
+
+
+printf("permission - %d\n",!is_permitted);
+
+
+    return 0;
+}
